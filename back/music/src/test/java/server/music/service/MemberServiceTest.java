f4887504d6c69e34ae4c377edc08f3c70eb1cf46
kangho <rkdgh98@khu.ac.kr>
2022-05-17 15:54:10 +0900

    test: 회원가입 테스트케이스 추가
    - 회원가입 엔티티 등록 확인
    - 회원 중복 예외처리 테스트

---
+++ back/music/src/test/java/server/music/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package server.music.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import server.music.domain.Member;
+import server.music.repository.MemberRepository;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+	@Autowired
+	MemberService memberService;
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Test
+	public void 회원가입() throws Exception {
+		//given
+		Member member = new Member();
+		member.setLogin_id("id");
+		member.setPassword("password");
+
+		//when
+		Long registerId = memberService.register(member);
+
+		//then
+		Member one = memberRepository.findOne(registerId);
+		assertThat(member.getId()).isEqualTo(one.getId());
+	}
+
+	@Test
+	public void 중복회원_검증() throws Exception {
+		//given
+		Member member1 = new Member();
+		Member member2 = new Member();
+		member1.setLogin_id("sameId");
+		member1.setPassword("password");
+		member2.setLogin_id("sameId");
+		member2.setPassword("password");
+
+		//when
+		memberService.register(member1);
+
+		//then
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			memberService.register(member2);
+		});
+	}
+}
