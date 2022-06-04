a037a934ead8cd1c236fbdffb87d2362a927e6f5
JoMars0722 <wt0722@khu.ac.kr>
2022-06-05 02:39:26 +0900

    add: 플레이리스트 재생버튼 추가

--- front/src/components/TrackContainer.js
+++ front/src/components/TrackContainer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { IoPlayCircle } from "react-icons/io5"
 import "../css/TrackContainer.css";
 import TrackCategory from "./TrackCategory";
 import TrackInfo from "./TrackInfo";
@@ -29,14 +30,24 @@ const playlist = {
 function TrackContainer(props) {
     const globalVar = useContext(AppContext);
 
+    const submitPlaylistToPlay = (value) => {
+        console.log(value);
+    }
+
     // 서버에 playlist id값으로 트랙 리스트 fetch
     return(
         <div className="track__container">
             <div className="playlist__info">
                 <img src={globalVar.selectedPlaylist.imageUrl} width='200' height='200' alt="cover"/>
                 <div className="playlist__info__name">
-                    {globalVar.selectedPlaylist.name}
+                    <p>{globalVar.selectedPlaylist.name}</p>
+                    <button onClick={ () => {
+                        submitPlaylistToPlay(globalVar.selectedPlaylist.name)
+                    }}>
+                        <IoPlayCircle className="playlist__info__play__circle" size='50' color="#1db954" />
+                    </button>
                 </div>
+                
             </div>
             <div className="playlist__tracks">
                 <div className="playlist__track__category">
