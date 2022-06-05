4bfe7b93aa1f11c5f1fcd384e18df8c7b696b52e
JoMars0722 <wt0722@khu.ac.kr>
2022-06-06 01:21:02 +0900

    feat: Post /playlists/{member_id}/create api 호출

--- front/src/components/PlaylistAddPopup.js
+++ front/src/components/PlaylistAddPopup.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import "../css/PlaylistAddPopup.css"
 
 function PlaylistAddPopup(props) {
@@ -6,8 +7,20 @@ function PlaylistAddPopup(props) {
     const handleInputPlaylistName = (e) => {
         setInputPlaylistName(e.target.value);
     }
+
+    const postPlaylistCreate = async (inputPlaylistName) => {
+        await axios.post("/playlists/"+sessionStorage.getItem('member_id')+'/create', {
+            member_id: sessionStorage.getItem('member_id'),
+            name: inputPlaylistName
+        }).then(function (res) {
+            console.log(res)
+        })
+    }
+
     const submitPlaylistName = () => {
         // fetch하고 창 닫기
+        //@PostMapping("/playlists/{member_id}/create")
+        postPlaylistCreate(inputPlaylistName);
         console.log(inputPlaylistName)
         props.close()
     }
