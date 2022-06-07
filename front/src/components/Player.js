31423090c8d403d26a0dda0fa7af1c53cb8668a4
JoMars0722 <wt0722@khu.ac.kr>
2022-06-08 02:37:46 +0900

    edit: Player 테스트 데이터 제거

--- front/src/components/Player.js
+++ front/src/components/Player.js
@@ -26,20 +26,7 @@ const makeTrackUriList = (tracks) => {
 
 function Player(props) {
     const config = require('../config.json');
-    const [uriList, setUriList] = useState(['spotify:track:11dFghVXANMlKmJXsNCbNl', 'spotify:track:1oc92BMKYkNxK17C2GtyRM'])
-    
-    const testTracks = [
-        {'id':'1', 'title':'title1', 'artist':'qasdf234we', 'album':'123', 'spotify_id': '0O6u0VJ46W86TxN9wgyqDj'},
-        {'id':'2', 'title':'asdqwr', 'artist':'qqwewe', 'album':'123', 'spotify_id': '1oc92BMKYkNxK17C2GtyRM'},
-        {'id':'3', 'title':'asdasxcb', 'artist':'qwerwe', 'album':'123', 'spotify_id': '64pa87zjoGMjhPifeIrzhb'},
-        {'id':'4', 'title':'asqwzxd', 'artist':'qasdfwe', 'album':'123', 'spotify_id': '0dgldEcoFDEbD4zCmyf9TT'},
-        {'id':'5', 'title':'asfagrfhtgykd', 'artist':'asdfzxcqwe', 'album':'123', 'spotify_id': '5eAVdKo6xpIVcrtmB2vyF4'},
-        {'id':'6', 'title':'asert2d', 'artist':'qwetwewe', 'album':'123', 'spotify_id': '6GnhWMhgJb7uyiiPEiEkDA'},
-        {'id':'7', 'title':'werwerasd', 'artist':'qxcvnswe', 'album':'123', 'spotify_id': '0d8anwJJGti8jE79Y4tXUD'},
-    ]
     const globalVar = useContext(AppContext);
-    
-
 
     console.log('Player Render', globalVar.selectedPlaylist)
 
