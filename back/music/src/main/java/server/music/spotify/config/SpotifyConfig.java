7418aa17853fcb17881053e82647fc825cc13340
kangho <rkdgh98@khu.ac.kr>
2022-06-15 17:21:03 +0900

    feat: spotify api key 숨기는 방식 변경

--- back/music/src/main/java/server/music/spotify/config/SpotifyConfig.java
+++ back/music/src/main/java/server/music/spotify/config/SpotifyConfig.java
@@ -1,24 +1,33 @@
 package server.music.spotify.config;
 
 import org.apache.hc.core5.http.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
 import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
-import server.music.spotify.api.ApiKey;
 
 import java.io.IOException;
 
 @Configuration
+@PropertySource("classpath:application-API-KEY.properties")
 public class SpotifyConfig {
+
+    @Value("${spotify.client.id}")
+    public static String SPOTIFY_CLIENT_ID;
+
+    @Value("${spotify.client.secret}")
+    public static String SPOTIFY_CLIENT_SECRET;
 
     @Bean
     public SpotifyApi spotifyApi(){
         SpotifyApi spotifyApi = new SpotifyApi.Builder()
-                .setClientId(ApiKey.SPOTIFY_CLIENT_ID)
-                .setClientSecret(ApiKey.SPOTIFY_CLIENT_SECRET)
+                .setClientId(SPOTIFY_CLIENT_ID)
+                .setClientSecret(SPOTIFY_CLIENT_SECRET)
                 .build();
         generateAccessToken(spotifyApi);
         return spotifyApi;
